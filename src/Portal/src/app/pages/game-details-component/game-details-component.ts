89170c36db0c231a86caad1834252585211a77ff
Maciej Świtalski <maciejswitalski7@gmail.com>
2026-03-19 20:54:45 +0100

    add fallback to header image

--- src/Portal/src/app/pages/game-details-component/game-details-component.ts
+++ src/Portal/src/app/pages/game-details-component/game-details-component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, input } from '@angular/core';
+import { Component, computed, inject, input, signal } from '@angular/core';
 import { DecimalPipe, NgOptimizedImage } from '@angular/common';
 import { toObservable, toSignal } from '@angular/core/rxjs-interop';
 import { switchMap } from 'rxjs';
@@ -13,9 +13,26 @@ import { RouterLink } from '@angular/router';
 })
 export class GameDetailsComponent {
   private readonly worthinatorService = inject(WorthinatorService);
-  id = input.required<string>();
 
+  id = input.required<string>();
   gameInfo = toSignal(
     toObservable(this.id).pipe(switchMap((id) => this.worthinatorService.getGameInfo(Number(id)))),
   );
+
+  readonly steamBaseUrl = 'https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/';
+  private useFallback = signal<boolean>(false);
+
+  bannerUrl = computed(() => {
+    const currentId = this.id();
+    if (this.useFallback()) {
+      return `${this.steamBaseUrl}${currentId}/header.jpg`;
+    }
+    return `${this.steamBaseUrl}${currentId}/library_hero.jpg`;
+  });
+
+  handleImageError() {
+    if (!this.useFallback()) {
+      this.useFallback.set(true);
+    }
+  }
 }
